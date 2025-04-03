8df3976a6a8b0d35cb26f506eab3d02b91664fad
Samuel Pitonak <development@samuelpitonak.sk>
2025-04-03 22:02:27 +0200

    fix: dash title

--- app/page.tsx
+++ app/page.tsx
@@ -38,7 +38,7 @@ export default function DashboardPage() {
   return (
     <main className="p-4">
       <div className="flex flex-col sm:flex-row sm:justify-between sm:items-center gap-2 mb-6">
-        <h1 className="text-xl font-bold">Free With Tech: Weekly Dashboard</h1>
+        <h1 className="text-xl font-bold">Free With Tech: Creator Dashboard</h1>
         <a
           href="https://www.buymeacoffee.com/sampittko"
           target="_blank"
